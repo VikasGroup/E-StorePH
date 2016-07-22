90427354460861c510396ec216a8ffde2e028429
Pavithri <Pavithri@Pavithri-PC>
2016-07-22 10:10:32 +0530

    tun test

--- src/main/java/com/pageObjects/PaymentDetailsPgObject.java
+++ src/main/java/com/pageObjects/PaymentDetailsPgObject.java
@@ -1,5 +1,40 @@
 package com.pageObjects;
 
-public class PaymentDetailsPgObject {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import com.base.Base;
+
+public class PaymentDetailsPgObject {
+	 static WebDriver driver;
+	 public Base base;
+	 public PaymentDetailsPgObject( WebDriver driver){
+		 PaymentDetailsPgObject.driver=driver;	 
+}
+	 public static void selectPayMethod(String value){
+			Select paymethod = new Select(driver.findElement(By.xpath(".//*[@id='MainContent_cboPaymentType']")));
+			paymethod.selectByIndex(Integer.parseInt(value));
+		} 
+	 public static void selectCurrency(String value){
+			Select currency = new Select(driver.findElement(By.xpath(".//*[@id='MainContent_cboddlCurrency']")));
+			currency.selectByIndex(Integer.parseInt(value));
+		} 
+	 public static void enterEcardNo(String value){
+			WebElement ecardNo = driver.findElement(By.xpath(".//*[@id='MainContent_eCardCtrl1_dgGrid_txtECardNo_0']"));
+			ecardNo.sendKeys(value);
+		}
+	 public static void enterPin(String value){
+			WebElement pin = driver.findElement(By.xpath(".//*[@id='MainContent_eCardCtrl1_dgGrid_txtECardPin_0']"));
+			pin.sendKeys(value);
+		}
+		public static void clickValidate(){
+			WebElement validate = driver.findElement(By.xpath(".//*[@id='MainContent_eCardCtrl1_dgGrid_cmdValidate_0']"));
+			validate.click();
+		}
+		public static void clickConfirm(){
+			WebElement confirm = driver.findElement(By.xpath(".//*[@id='MainContent_eCardCtrl1_cmdOK']"));
+			confirm.click();
+		}
 }
--- src/main/java/com/pageObjects/TunisiaEnvPgObject.java
+++ src/main/java/com/pageObjects/TunisiaEnvPgObject.java
@@ -5,132 +5,146 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-public class PreregisterTunisiaPgObject {
+public class TunisiaEnvPgObject {
 	static WebDriver driver;
 	
-	public PreregisterTunisiaPgObject(WebDriver driver){
-		PreregisterTunisiaPgObject.driver = driver;	
+	public TunisiaEnvPgObject(WebDriver driver){
+		TunisiaEnvPgObject.driver = driver;	
 	}
 //Step1
-	public static void enterReferrer(String value){
+	/*public  void enterReferrer(String value){
 		WebElement referrerIrID = driver.findElement(By.xpath(".//*[@id='txtReferrer']"));
 		referrerIrID.sendKeys(value);
 	}
 	
-	public static void selectCountry(String value){
+	public  void selectCountry(String value){
 		Select dpCountry= new Select(driver.findElement(By.xpath(".//*[@id='ddlCountry']")));
 		dpCountry.selectByIndex(Integer.parseInt(value));
 		
 	}
 
-	public static void clickcontinue1(){
+	public  void clickcontinue1(){
 		WebElement step1countinue = driver.findElement(By.xpath(".//*[@id='btnVerifyReferrer']"));
 		step1countinue.click();
-	}
+	}*/
 	//step2
-	public static void selectTitle(String value){
+	public  void selectTitle(String value){
 		Select dpTitle = new Select(driver.findElement(By.xpath(".//*[@id='ddlTitle']")));
 		dpTitle.selectByIndex(Integer.parseInt(value));
 	}
 	
-	public static void enterfirstName(String value){
+	public  void enterSurname(String value){
 		WebElement fName = driver.findElement(By.xpath(".//*[@id='txtFirstName']"));
 		fName.sendKeys(value);
 	}
 	
-	public static void enterlastName(String value){
+	
+	public  void enterGiveName(String value){
 		WebElement lName = driver.findElement(By.xpath(".//*[@id='txtLastName']"));
 		lName.sendKeys(value);
 	}
+	public  void enterArabname(String value){
+		WebElement aName = driver.findElement(By.xpath(".//*[@id='tbFullName']"));
+		aName.sendKeys(value);
+	}
 	
-	public static void selectdobMonth(String value){
+	public  void selectdobmonth(String value){
 		Select dpMonth = new Select(driver.findElement(By.xpath(".//*[@id='ddlMonth']")));
 		dpMonth.selectByIndex(Integer.parseInt(value));
 	}
 	
-	public static void selectdobDate(String value){
+	public  void selectdobdate(String value){
 		Select dpDate = new Select(driver.findElement(By.xpath(".//*[@id='ddlDate']")));
 		dpDate.selectByIndex(Integer.parseInt(value));
 	}
 	
-	public static void selectdobYear(String value){
+	public  void selectdobyear(String value){
 		Select dpYear = new Select(driver.findElement(By.xpath(".//*[@id='ddlYear']")));
 		dpYear.selectByIndex(Integer.parseInt(value));
 	}
 	
-	public static void enteremail(String value){
+	public  void enterEmail(String value){
 		WebElement emailAdd = driver.findElement(By.xpath(".//*[@id='txtEmailAdd']"));
 		emailAdd.sendKeys(value);
 	}
 	
-	public static void enterreTypeEmail(String value){
+	public  void ReEnterEmail(String value){
 		WebElement reEmail  = driver.findElement(By.xpath(".//*[@id='txtEmailAddConfirmation']"));
 		reEmail.sendKeys(value);	
 	}
 	
-	public static void selectContNo(String value){
+	public  void selectContNo(String value){
 		Select dpContactNum = new Select(driver.findElement(By.xpath(".//*[@id='ddlPreferredContact']")));
 		dpContactNum.selectByIndex(Integer.parseInt(value));
 	}
-	public static void entercontNo(String value){
+	public  void entercontNo(String value){
 		WebElement contactNo = driver.findElement(By.xpath(".//*[@id='txtPreferredContactNo']"));
 		contactNo.sendKeys(value);	
 	}
-	public static void enterpassword(String value){
+	public  void enterPwd(String value){
 		WebElement passWord  = driver.findElement(By.xpath(".//*[@id='txtPassword']"));
 		passWord.sendKeys(value);	
 	}
-	public static void reenterPassword(String value){
+	public  void reenterPwd(String value){
 		WebElement repwd = driver.findElement(By.xpath(".//*[@id='txtReTypePassword']"));
 		repwd.sendKeys(value);	
 	}
-	public static void enteraddline1(String value){
+	public  void EnterAddress(String value){
 		WebElement addl1 = driver.findElement(By.xpath(".//*[@id='txtaddrss1']"));
 		addl1.sendKeys(value);	
 	}
-	public static void entertown(String value){
+	public  void enterTown(String value){
 		WebElement city = driver.findElement(By.xpath(".//*[@id='txttown']"));
 		city.sendKeys(value);	
 	}
-	public static void enterzipCode(String value){
+	public  void enterPostal(String value){
 		WebElement zipcode = driver.findElement(By.xpath(".//*[@id='txtzip']"));
 		zipcode.sendKeys(value);	
 	}
-	public static void clickcontiune2(){
+	public  void clickcontiune2(){
 		WebElement step2countinue = driver.findElement(By.xpath(".//*[@id='MainContent_btnRegister']"));
 		step2countinue.click();
 	}
 	
 	//Step3
-	public static void enteriridNum(String value){
+	public  void enterIRID(String value){
 		WebElement IR_ID= driver.findElement(By.xpath(".//*[@id='btnVerifyPlacement']"));
 		IR_ID.sendKeys(value);
 	}
 	
-	public static void clickPlacement(){
+	public  void clickVerifyBtn(){
 		WebElement verifyPlacement = driver.findElement(By.xpath(".//*[@id='btnVerifyPlacement']"));
 		verifyPlacement.click();
 	}
 	
-	public static void selecttrackCentre(String value){
+	public  void selectPlacement(String value){
 		Select dpTraCentre = new Select(driver.findElement(By.xpath(".//*[@id='ddlTCExt']")));
 		dpTraCentre.selectByIndex(Integer.parseInt(value));
 	}
-	public static void clickLeftTC(){
+	public void prefferPlacement(String value){
+		 WebElement right=driver.findElement(By.xpath(".//*[@id='radRight']"));
+		 WebElement left=driver.findElement(By.xpath(".//*[@id='radLeft']"));
+		 if(value.equals("right")){
+			 right.click();
+		 }else if(value.equals("left")){
+			 left.click();			 
+		 }
+	}
+	/*public  void clickLeftTC(){
 		WebElement leftTC = driver.findElement(By.xpath(".//*[@id='radLeft']"));
 		leftTC.click();
 	}
-	public static void clickRightTC(){
+	public  void clickRightTC(){
 		WebElement rightTC = driver.findElement(By.xpath(".//*[@id='radRight']"));
 		rightTC.click();
-	}
-	public static void clickCountinue3(){
+	}*/
+	public  void clickCountinue3(){
 		WebElement countinue3 = driver.findElement(By.xpath(".//*[@id='MainContent_btnContinuePlacement']"));
 		countinue3.click();
 	}
 	
 	//Step 4
-	public static void clickSubmit(){
+	public  void clickSubmit(){
 		WebElement submit = driver.findElement(By.xpath(".//*[@id='btnSubmit']"));
 		submit.click();
 	}
